843a5a864dab171e56e3a77da58e32dc26206f1f
yxkumad <46276046+yxkumad@users.noreply.github.com>
2021-03-02 15:59:34 +0800

    华为云 DNS 专用
    2021-03-02 1.1.0
    汉化&增加故障累计

--- dns_ls_huaweicloud.sh
+++ dns_ls_huaweicloud.sh
@@ -3,49 +3,68 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# Place at:
-# curl https://raw.githubusercontent.com/yxkumad/dns_lb/master/dns_lb_huaweicloud.sh > /usr/local/bin/dns_lb_huaweicloud.sh && chmod +x /usr/local/bin/dns_lb_huaweicloud.sh
-# run `crontab -e` and add next line:
-# */1 * * * * /usr/local/bin/dns_lb_huaweicloud.sh >/dev/null 2>&1
-# or you need log:
-# */1 * * * * /usr/local/bin/dns_lb_huaweicloud.sh >> /var/log/dns_lb_huaweicloud.log 2>&1
-
 # Configuration
+echo " *******   ****     **  ********       **       ******  
+/**////** /**/**   /** **//////       /**      /*////** 
+/**    /**/**//**  /**/**             /**      /*   /** 
+/**    /**/** //** /**/*********      /**      /******  
+/**    /**/**  //**/**////////**      /**      /*//// **
+/**    ** /**   //****       /**      /**      /*    /**
+/*******  /**    //*** ********  *****/********/******* 
+///////   //      /// ////////  ///// //////// ///////  "
+echo "Github: https://github.com/yxkumad/dns_lb"
+echo "Telegram: https://t.me/yxkumad"
+echo "正在初始化..."
 
-# Ping API
-PING_API=http://yourapi/ping
-# Original IP
-ORG_IP=
+# Ping API 使用 https://github.com/TorchPing/go-torch 搭建，新版本脚本只需要输入 API 的 IP 和 端口（默认 8080）
+PING_API=x.x.x.x:8080
+echo "您的 Ping API $PING_API"
 
-# Failure IP
-FAIL_IP=
+# 原服务器 IP
+ORG_IP=x.x.x.x
+echo "您的原服务器 IP: $ORG_IP"
 
-# Telegram Bot Token
+# 备用服务器 IP
+FAIL_IP=x.x.x.x
+echo "您的备用服务器 IP: $FAIL_IP"
+
+# 端口（默认为 22）
+PORT=22
+
+# Telegram 机器人 Token
 TG_BOT_TOKEN=
 
-# Telegram Chat ID
+# Telegram 发送用户/群组/频道 ID
 TG_CHATID=
 
-#用户名
+#华为云用户名
 username=""
-#账户名
+
+#华为云账户名
 accountname=""
-#密码
+
+#华为云密码
 password=""
 
-#域名
-domain="example.com"
-#主机名
-host="www"
+#域名（如 985.moe）
+domain=""
 
-#End Point 终端地址 请根据地域选择
+#DNS 段名（如 www）
+host=""
+
+echo "您的域名: $host.$domain"
+
+#华为云 IAM API 终端地址 请根据地域选择
 iam="iam.myhuaweicloud.com"
 #iam="iam.ap-southeast-1.myhuaweicloud.com"
 #iam="iam.ap-southeast-3.myhuaweicloud.com"
 
+#华为云 DNS API 终端地址 请根据地域选择
 dns="dns.myhuaweicloud.com"
 #dns="dns.ap-southeast-1.myhuaweicloud.com"
 #dns="dns.ap-southeast-3.myhuaweicloud.com"
+
+echo "正在获取华为云 Token..."
 
 token_X="$(
     curl -L -k -s -D - -X POST \
@@ -76,6 +95,8 @@ token_X="$(
 
 token="$(echo $token_X | awk -F ' ' '{print $2}')"
 
+echo "获取华为云 Token 完成"
+
 recordsets="$(
     curl -L -k -s -D - \
         "https://$dns/v2/recordsets?name=$host.$domain." \
@@ -86,52 +107,63 @@ recordsets="$(
 RECORDSET_ID=$(echo $recordsets | cut -d ' ' -f 1)
 ZONE_ID=$(echo $recordsets | cut -d ' ' -f 2 | cut -d ' ' -f 2)
 
-
-# Get current and old WAN ip
+# 存放 IP 文件
 PRESENT_IP_FILE=$HOME/.ip_$host.$domain.txt
 if [ -f $PRESENT_IP_FILE ]; then
   OLD_PRESENT_IP=`cat $PRESENT_IP_FILE`
 else
-  echo "No file, need IP"
+  echo "不存在 IP 文件，正在建立"
   OLD_PRESENT_IP=""
 fi
 
-# Check service failure
-CHECK=$(curl -s "$PING_API/$ORG_IP/22")
+# 检测服务器故障
+echo "正在检测服务器 $ORG_IP 的状态..."
+CHECK=$(curl -s "http://$PING_API/ping/$ORG_IP/$PORT")
+
+# 存放故障次数文件
+ERROR=$HOME/.error_$host.$domain.txt
 
 if [ "$(echo $CHECK | grep "\"status\":true")" != "" ]; then
   if [ "$ORG_IP" = "$OLD_PRESENT_IP" ]; then
-    echo "No service failure found. No DNS record update required. "
+    echo "原服务器 $ORG_IP 的状态为正常，无需切换 DNS 解析至备用服务器 $FAIL_IP"
     exit 0
   fi
-  echo "No service failure found. Updating DNS to $ORG_IP"
+  echo "原服务器 $ORG_IP 的状态已回复正常，正在切换 DNS 解析至原服务器 $ORG_IP"
   RESPONSE=$(curl -X PUT -L -k -s \
     "https://$dns/v2/zones/$ZONE_ID/recordsets/$RECORDSET_ID" \
     -H "Content-Type: application/json" \
     -H "X-Auth-Token: $token" \
     -d "{\"records\": [\"$ORG_IP\"],\"ttl\": 1}")  
-  curl -s "https://api.telegram.org/bot$TG_BOT_TOKEN/sendMessage?chat_id=$TG_CHATID&text=No service failure found. Updating DNS record $host.$domain to $ORG_IP"
+  echo "正在发送信息到 Telegram..."
+  curl -s "https://api.telegram.org/bot$TG_BOT_TOKEN/sendMessage?chat_id=$TG_CHATID&text=域名 $host.$domain 的原服务器 $ORG_IP 的状态已回复正常，正在切换 DNS 解析至原服务器 $ORG_IP"
+  echo "0" > $ERROR 
   echo $ORG_IP > $PRESENT_IP_FILE
 else
   if [ "$FAIL_IP" = "$OLD_PRESENT_IP" ]; then
-    echo "Service failure found. No DNS record update required. "
+    echo "原服务器 $ORG_IP 的状态为异常，已于过去切换 DNS 解析至备用服务器 $FAIL_IP，无需再次切换"
+        exit 0
+  elif [ `cat $ERROR` -eq 1 ]; then
+    echo "原服务器 $ORG_IP 的状态为异常，并已累计 1 次故障次数，正在切换 DNS 解析至备用服务器 $FAIL_IP"
+    RESPONSE=$(curl -X PUT -L -k -s \
+      "https://$dns/v2/zones/$ZONE_ID/recordsets/$RECORDSET_ID" \
+      -H "Content-Type: application/json" \
+      -H "X-Auth-Token: $token" \
+      -d "{\"records\": [\"$FAIL_IP\"],\"ttl\": 1}") 
+    echo "正在发送信息到 Telegram..."
+    curl -s "https://api.telegram.org/bot$TG_BOT_TOKEN/sendMessage?chat_id=$TG_CHATID&text=域名 $host.$domain 的原服务器 $ORG_IP 的状态为异常并已累计 1 次故障次数，正在切换 DNS 解析至备用服务器 $FAIL_IP"
+    echo $FAIL_IP > $PRESENT_IP_FILE
+  else
+    echo "1" > $ERROR  
+    echo "原服务器 $ORG_IP 的状态为异常，已累计 1 次故障次数"
     exit 0
   fi
-  echo "Service failure found. Updating DNS to $FAIL_IP"
-  RESPONSE=$(curl -X PUT -L -k -s \
-    "https://$dns/v2/zones/$ZONE_ID/recordsets/$RECORDSET_ID" \
-    -H "Content-Type: application/json" \
-    -H "X-Auth-Token: $token" \
-    -d "{\"records\": [\"$FAIL_IP\"],\"ttl\": 1}") 
-  curl -s "https://api.telegram.org/bot$TG_BOT_TOKEN/sendMessage?chat_id=$TG_CHATID&text=Service failure found. Updating DNS record $host.$domain to $FAIL_IP"
-  echo $FAIL_IP > $PRESENT_IP_FILE
 fi
 
 if [ "$RESPONSE" != "${RESPONSE%success*}" ] && [ "$(echo $RESPONSE | grep "\"success\":true")" != "" ]; then
-  echo "Updated succesfuly!"
+  echo "切换 DNS 解析成功！"
   exit
 else
-  echo 'Something went wrong :('
-  echo "Response: $RESPONSE"
+  echo '切换 DNS 解析失败！'
+  echo "错误: $RESPONSE"
   exit 1
 fi
